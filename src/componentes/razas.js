7ba419c6ac5dbdd49f156c4d92c1be2a18519125
David <caico@fibertel.com.ar>
2021-10-20 20:06:54 -0300

    4to. commit  reemplazo select en componente raza por un autoselect

--- src/componentes/razas.js
+++ src/componentes/razas.js
@@ -5,26 +5,19 @@ import { makeStyles } from '@mui/material/styles';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import { red } from '@mui/material/colors';
 import axios  from 'axios';
-
-
-
-// const useStyles = makeStyles((theme) => ({
-//     formControl: {
-//       margin: theme.spacing(1),
-//       minWidth: 120,
-//     },
-//     selectEmpty: {
-//       marginTop: theme.spacing(2),
-//     },
-// }));
 
   
 const Razas = ({raza,changeRaza}) => {
 
     //const classes = useStyles();    
     const [listadoRazas,setListadoRazas] = useState([])
+    const [seleccion, setSeleccion] = useState('')
+
+  
 
     //Obtiene el listado de todas las razas
     const obtenerListadoRazas = () =>{
@@ -47,15 +40,37 @@ const Razas = ({raza,changeRaza}) => {
         obtenerListadoRazas()        
     },[]);
 
-    
-   
-
     return(
         <div>
             {/* <h4>Buscar razas</h4> */}
             {/* <FormControl className={classes.formControl}> */}
-            <FormControl fullWidth>   
-                <InputLabel id="selctLabelName" sx={{ mt: 3}}>Raza</InputLabel>
+           
+           <FormControl fullWidth>
+
+            <Autocomplete
+                disablePortal
+                id="combo-box-raza"
+                options={listadoRazas}
+                value={raza}
+                onChange={(event,newValue) =>{
+                    if(newValue == null){
+                        newValue = ""
+                    }
+                    changeRaza(newValue)
+                }}
+                onInputChange={ (e) => {
+                    if(e != null){
+                        changeRaza(e.target.value)
+                    }                    
+                }}             
+               
+                
+                sx={{ mb: 2, mt: 5, height: 35 }}
+                renderInput={(params) => <TextField {...params} label="Raza" />}
+            />
+
+
+                {/* <InputLabel id="selctLabelName" sx={{ mt: 3}}>Raza</InputLabel>
                 <Select
                     sx={{ mb: 2, mt: 5, height: 35}}
                     labelId = "selectLabelName"
@@ -63,6 +78,8 @@ const Razas = ({raza,changeRaza}) => {
                     value={raza}
                     lsabel="Raza"
                     onChange={ (e) => {changeRaza(e.target.value)}}
+                  
+                    
 
                 >
                     {listadoRazas.map((raza) => (
@@ -71,8 +88,8 @@ const Razas = ({raza,changeRaza}) => {
                         </MenuItem>
                     ))}
 
-                </Select>
-                
+                </Select> */}
+                 
             </FormControl>
         </div>
     );
